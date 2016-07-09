b082af3463ae3c8d8437a423d50fcd4b1c4b55da
Khalifa Sarwashi <khalifasarwashi@macbook-air-alkhas-b-khalifa.local>
2016-07-10 00:20:25 +0400

    Initial Commit

---
+++ snapkhalifa/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  snapkhalifa
+//
+//  Created by Khalifa  sarwashi on 10/07/2016.
+//  Copyright © 2016 Khalifa  sarwashi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ snapkhalifa/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  snapkhalifa
+//
+//  Created by Khalifa  sarwashi on 10/07/2016.
+//  Copyright © 2016 Khalifa  sarwashi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
